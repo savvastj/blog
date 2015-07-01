b9619864ad0daa9f2d9811d510f8f78b6ff3dd03
savvastj <savvas42091@gmail.com>
2015-07-01 14:07:33 -0400

    trying to get disqus to show and edited 2nd post

--- pelicanconf.py
+++ pelicanconf.py
@@ -17,18 +17,6 @@ DEFAULT_LANG = 'en'
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
-
-# Blogroll
-# LINKS = (('Pelican', 'http://getpelican.com/'),
-#          ('Python.org', 'http://python.org/'),
-#          ('Jinja2', 'http://jinja.pocoo.org/'),
-#          ('You can modify those links in your config file', '#'),)
-
-# Social widget
-# SOCIAL = (('You can add links in your config file', '#'),
-          # ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 5
 
@@ -77,5 +65,5 @@ STATIC_PATHS = ['images', 'code', 'extra/CNAME']
 EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'}, }
 
 
-SHARIFF = True
-SHARIFF_LANG = True
+# SHARIFF = True
+# SHARIFF_LANG = True
